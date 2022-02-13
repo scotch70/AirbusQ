ab8e2f73b76d431521af2cea1ee21566d3e0f898
Lars Neeft <lars_neeft@live.nl>
2022-02-13 16:50:45 +0100

    Initial Commit

---
+++ AirbusQ/AirbusQApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbusQApp.swift
+//  AirbusQ
+//
+//  Created by Lars Neeft on 13/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AirbusQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbusQ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirbusQ
+//
+//  Created by Lars Neeft on 13/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
